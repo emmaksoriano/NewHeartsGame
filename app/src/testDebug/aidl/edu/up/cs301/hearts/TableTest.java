b0d6cdee2ce1e10c292dc99362fbf05590339e0e
emmaksoriano <sorianoe19@up.edu>
2017-11-21 19:38:28 -0800

    Initial commit

---
+++ app/src/testDebug/aidl/edu/up/cs301/hearts/TableTest.java
@@ -0,0 +1,32 @@
+package edu.up.cs301.hearts;
+
+import junit.framework.TestCase;
+
+import edu.up.cs301.game.GameComputerPlayer;
+
+/**
+ * Created by lytle on 11/21/2017.
+ */
+public class TableTest extends TestCase {
+
+    public void testAddCard() throws Exception {
+
+    }
+
+    public void testGetTable() throws Exception {
+
+    }
+
+    public void testGetSuitIndex() throws Exception {
+
+    }
+
+    public void testGetHighestCard() throws Exception {
+
+    }
+
+    public void testClearTable() throws Exception {
+
+    }
+
+}
